033030a5c2f61a3e309cdc67851ac90485494e96
Mandeepsgm <mandeepsg.m@gmail.com>
2022-07-26 00:48:30 -0400

    added styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './style/App.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
@@ -17,17 +18,24 @@ class App extends React.Component{
         console.log("component has updated- it rerendered");
     }
 
-    render(){
-        
-            if(this.state.errorMessage && !this.state.lat){
-               return <div>Error: {this.state.errorMessage}</div>;
-            }
+    renderContent(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>;
+         }
 
-            if(!this.state.errorMessage && this.state.lat){
-                return <SeasonDisplay lat={this.state.lat} />;
-            }
+         if(!this.state.errorMessage && this.state.lat){
+             return <SeasonDisplay lat={this.state.lat} />;
+         }
+         
+         return <Spinner message="Please accept location request..." />;
+    }
+    render(){
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
             
-            return <Spinner message="Please accept location request..." />;
     }
 };
 
