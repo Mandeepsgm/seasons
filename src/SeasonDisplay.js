eaca1176ef0f80d605d5f82ceb0fe3a6c39188ce
Mandeepsgm <mandeepsg.m@gmail.com>
2022-07-26 00:03:01 -0400

    displaying correct season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+const getSeason = (lat, month) =>{
+    if(month > 2 && month <9){
+       return lat > 0 ? 'summer' : 'winter';
+    } else{
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
 const SeasonDisplay= (props) => {
-return <div>Season Display {props.lat}</div>;
+    const season = getSeason(props.lat, new Date().getMonth());
+
+return <div> <h1>{season === 'winter' ? 'Burr, it is chilly' : 'Lets hi tthe beach'}</h1></div>;
 
 };
 
